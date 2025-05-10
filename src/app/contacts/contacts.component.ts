a577b9bb5284b38005ab7a7da60a166388e85309
Sport97 <por21022@byui.edu>
2025-05-09 20:25:50 -0500

    contacts databinding

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contacts',
   standalone: false,
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.css'
+  styleUrl: './contacts.component.css',
 })
 export class ContactsComponent {
-
+  selectedContact!: Contact;
 }
